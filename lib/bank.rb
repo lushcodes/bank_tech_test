29ec1386df951fad3fa43b4b268eb8ca30a30799
lushcodes <lushcodes@gmail.com>
2021-08-31 12:30:54 +0100

    Method tests respond in class

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,9 +3,15 @@ class Bank
   attr_reader :balance
   def initialize
     @balance = 0
-    
+
   end
 
+  def deposit(num)
 
+  end
+
+  def withdraw(num)
+    
+  end
 end
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,10 +1,19 @@
 require 'bank'
 
 describe Bank do
+  
+  it { is_expected.to respond_to(:deposit).with(1).argument }
 
   context 'when initialised' do
+
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+
     it 'has an initial balance of 0' do
       expect(subject.balance).to eq(0)
     end
   end
+
+
 end
